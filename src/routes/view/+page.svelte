5307a33803f7084ee09c30c256cb8242e857058d
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2024-07-27 19:55:42 +0700

    add persistence states.

--- src/routes/view/+page.svelte
+++ src/routes/view/+page.svelte
@@ -18,13 +18,17 @@
 	import { get } from 'svelte/store';
 
 	onMount(() => {
-        console.log($comfyUrlWritable, $pushyAppIdWritable);
+		comfyURL = get(comfyUrlWritable);
+		pushyAppId = get(pushyAppIdWritable);
 	});
 
 	function onStartBtn() {
-        console.log($comfyUrlWritable, $pushyAppIdWritable);
+		comfyUrlWritable.set(comfyURL);
+		pushyAppIdWritable.set(pushyAppId);
 	}
 
+	let comfyURL = '';
+	let pushyAppId = '';
 </script>
 
 <Navbar dark expand="md" container="md">
@@ -41,7 +45,7 @@
 	<Form>
 		<FormGroup>
 			<Label>ComfyUI URL</Label>
-			<Input type="url" placeholder="http://127.0.0.1:8188/" bind:value={$comfyUrlWritable} />
+			<Input type="url" placeholder="http://127.0.0.1:8188/" bind:value={comfyURL} />
 		</FormGroup>
 	</Form>
 
@@ -49,7 +53,7 @@
 	<Form>
 		<FormGroup>
 			<Label>App ID</Label>
-			<Input type="text" placeholder="ffffffffffffffffffffffff" bind:value={$pushyAppIdWritable} />
+			<Input type="text" placeholder="ffffffffffffffffffffffff" bind:value={pushyAppId} />
 		</FormGroup>
 	</Form>
 </Container>
