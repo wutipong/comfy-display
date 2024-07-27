c54677d60a9ec77f8e31cbc5633fd13a85b23b6b
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2024-07-28 05:36:00 +0700

    remove persistence storage, use env

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,70 +3,23 @@
 		Navbar,
 		NavbarBrand,
 		Nav,
-		NavItem,
-		NavLink,
-		Icon,
-		Form,
-		FormGroup,
-		Input,
-		Label,
-		Container,
-		Table,
-		Button
+		Container
 	} from '@sveltestrap/sveltestrap';
+	import type { PageData } from './$types';
 
-	import { onMount } from 'svelte';
-	import { goto } from '$app/navigation';
-	import { comfyUrlWritable, comfyHeadersWritable, pushyAppIdWritable } from '$lib/store';
-
-	onMount(() => {});
-
-	function onStartBtn() {
-		goto('/view');
-	}
+	export let data: PageData;
 </script>
 
 <Navbar dark expand="md" container="md">
 	<NavbarBrand href="/">ComfyUI Display</NavbarBrand>
-	<Nav class="ms-auto" navbar>
-		<NavItem>
-			<NavLink on:click={onStartBtn}><Icon name="play" />&nbsp;Begin</NavLink>
-		</NavItem>
-	</Nav>
+	<Nav class="ms-auto" navbar></Nav>
 </Navbar>
 
 <Container>
-	<h2>ComfyUI Settings</h2>
-	<Form>
-		<FormGroup>
-			<Label>ComfyUI URL</Label>
-			<Input type="url" placeholder="http://127.0.0.1:8188/" bind:value={$comfyUrlWritable} />
-		</FormGroup>
-		<Label>Request Headers</Label>
-		<Table>
-			<thead>
-				<th>Field</th>
-				<th>Value</th>
-				<th>Action</th>
-			</thead>
-			<tbody>
-				{#each $comfyHeadersWritable as header}
-					<tr>
-						<td><Input bind:value={header.field} /></td>
-						<td><Input bind:value={header.value} /></td>
-						<td><Button><Icon name="minus" /></Button></td>
-					</tr>
-				{/each}
-			</tbody>
-		</Table>
-		<Button><Icon name="plus"></Icon></Button>
-	</Form>
-
-	<h2>Pushy Settings</h2>
-	<Form>
-		<FormGroup>
-			<Label>App ID</Label>
-			<Input type="text" placeholder="ffffffffffffffffffffffff" bind:value={$pushyAppIdWritable} />
-		</FormGroup>
-	</Form>
+	{#if data.imageUrl.length === 0}
+		<p>Emtpy data. Wait for incoming notifications.</p>
+	{/if}
+	{#each data.imageUrl as imageUrl}
+		<img src={imageUrl.toString()}/>
+	{/each}
 </Container>
--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,47 +1,51 @@
-import { comfyUrlWritable, comfyHeadersWritable } from '$lib/store'
-import { get } from 'svelte/store'
+import { env } from '$env/dynamic/public';
 
-export const ssr = false;
+let obj = {};
+if(env.PUBLIC_COMFY_HEADERS !== undefined){
+    obj = JSON.parse(env.PUBLIC_COMFY_HEADERS)
+}
+
+const comfyHeaders = new Map<string, string> ( Object.entries(obj));
+const comfyUrl = env.PUBLIC_COMFY_URL;
 
 export interface PageData {
     imageUrl: URL[]
 }
 
 export async function load({ fetch, params }): Promise<PageData> {
-    const comfyUrl = get(comfyUrlWritable)
-    const comfyHeaders = get(comfyHeadersWritable)
     const historyUrl = new URL('/history', comfyUrl)
     historyUrl.searchParams.append('max_items', '1');
 
     try {
         const request = new Request(historyUrl);
 
-        /*
-        for(const h of comfyHeaders){
-            request.headers.append(h.field, h.value);
-        }*/
-        
+        for (const k of comfyHeaders.keys()) {
+            const v = comfyHeaders.get(k);
+            if (v !== undefined)
+                request.headers.append(k, v);
+        }
+
         const data = await fetch(request)
         const obj = await data.json();
 
         const history = obj[Object.keys(obj)[0]];
         const outputs = history["outputs"];
-        if (outputs === undefined){
+        if (outputs === undefined) {
             throw new Error()
         }
         const outputKeys = Object.keys(outputs);
 
         let images = [];
-        
-        for(const k of outputKeys){
-            for(const i of outputs[k].images){
+
+        for (const k of outputKeys) {
+            for (const i of outputs[k].images) {
                 images.push(i)
             }
         }
 
         let imageUrl = [];
-        for(const i of images){
-            const url = new URL('/view',comfyUrl);
+        for (const i of images) {
+            const url = new URL('/view', comfyUrl);
             url.searchParams.append('filename', i.filename);
             url.searchParams.append('type', i.type);
             url.searchParams.append('subfolder', i.subfolder);
