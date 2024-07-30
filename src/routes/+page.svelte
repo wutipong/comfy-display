0793af0414767fb2e5083e34693162d33991f216
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2024-07-31 01:56:56 +0700

    change how warning displays.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import {
-		Button,
+		Card,
+		CardHeader,
+		CardBody,
 		Container,
 		Icon,
 		Nav,
@@ -13,20 +15,18 @@
 	import { onMount, onDestroy } from 'svelte';
 	import { invalidateAll } from '$app/navigation';
 	import { comfyURL } from '$lib/config';
-
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
 	let activeIndex = 0;
-
 	let carousel: bootstrap.Carousel;
 	let refreshInterval: number;
 
 	const pollInterval = 15_000;
 
 	onMount(async () => {
-		const bootstrap = await import('bootstrap')
+		const bootstrap = await import('bootstrap');
 
 		refreshInterval = setInterval(() => invalidateAll(), pollInterval);
 
@@ -39,8 +39,6 @@
 				ride: 'carousel'
 			});
 		}
-
-		console.log('mount')
 	});
 
 	onDestroy(() => {
@@ -71,6 +69,11 @@
 	<NavbarBrand href="/">ComfyUI Display</NavbarBrand>
 	<Nav class="ms-auto" navbar>
 		<NavItem>
+			<NavLink on:click={togglePause}>
+				<Icon bind:name={pauseIcon} />
+			</NavLink>
+		</NavItem>
+		<NavItem>
 			<NavLink href={comfyURL} target="_blank">
 				<Icon name="robot" />
 				&nbsp;ComfyUI
@@ -82,11 +85,25 @@
 {#if data.imageUrls.length === 0}
 	<Container>
 		{#if data.error === undefined}
-			<p>Latest workflow does not contains images or no workflow has been running.</p>
+			<Card color="warning">
+				<CardHeader>
+					<Icon name="exclamation-circle" />&nbsp; Workflow contains no images
+				</CardHeader>
+				<CardBody>
+					Latest workflow does not contains images or no workflow has been running.
+				</CardBody>
+			</Card>
 		{/if}
 
 		{#if data.error !== undefined}
-			<p>{data.error}</p>
+			<Card color="danger">
+				<CardHeader>
+					<Icon name="exclamation-triangle-fill" />&nbsp; Cannot connect to ComfyUI
+				</CardHeader>
+				<CardBody>
+					{data.error}
+				</CardBody>
+			</Card>
 		{/if}
 	</Container>
 {/if}
@@ -95,9 +112,6 @@
 	<div class="fullscreen" style="padding-top:60px;">
 		<div class="carousel slide w-100 h-100" id="carouselControl">
 			<div class="carousel-indicators">
-				<Button on:click={togglePause}>
-					<Icon bind:name={pauseIcon}></Icon>
-				</Button>
 				{#each data.imageUrls as _, index}
 					<button
 						type="button"
@@ -109,20 +123,18 @@
 					></button>
 				{/each}
 			</div>
-			
+
 			<div class="carousel-inner w-100 h-100" id="carousel" style="width:100%; height:100%;">
 				{#each data.imageUrls as url, index}
 					<div class="carousel-item w-100 h-100" class:active={index === activeIndex}>
-						<div class="w-100 h-100 d-flex flex-col">
-							<a href={url.toString()} target="_blank">
-								<img
-									class="ms-auto me-auto w-100 h-100"
-									alt="page {index}"
-									src={url.toString()}
-									style="object-fit:contain;max-width:100%;max-height:100%"
-								/>
-							</a>
-						</div>
+						<a href={url.toString()} target="_blank">
+							<img
+								class="ms-auto me-auto w-100 h-100"
+								alt="page {index}"
+								src={url.toString()}
+								style="object-fit:contain;max-width:100%;max-height:100%"
+							/>
+						</a>
 					</div>
 				{/each}
 			</div>
